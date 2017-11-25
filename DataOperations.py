b263a0db89792a23ca71b1e3694f5bee84893d4e
ankitmohla24 <ankitmohla24@gmail.com>
2017-11-25 01:33:29 -0600

    Data operations

--- DataOperations.py
+++ DataOperations.py
@@ -5,3 +5,13 @@ print(num_1 + num_2)  #integer sum
 num_3 = "3"
 num_4 = "4"
 print (num_3 + num_4) #string concat
+
+minutes = 60
+#minutes per hour
+hours = 24
+#hours per day
+days = 24
+#days per week
+
+#print minutes per day
+print minutes*hours
