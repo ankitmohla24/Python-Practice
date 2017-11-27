5c1adbd5a3f354704ab994f6383a68b69ccfeb39
ankitmohla24 <ankitmohla24@gmail.com>
2017-11-26 19:37:06 -0600

    loops

---
+++ Loops.py
@@ -0,0 +1,19 @@
+def whileloop(n):
+    x=0
+    while x < n:
+        print "use of while",
+        print x,
+        print "times"
+        x = x+1
+
+
+def forloop(n):
+    for x in range(0, n):
+        print "use of for",
+        print x,
+        print "times"
+
+
+number = input("enter a number ")
+whileloop(number)
+forloop(number)
